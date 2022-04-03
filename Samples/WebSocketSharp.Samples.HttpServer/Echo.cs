e6f83d2d21c4c66c139d623fd697273b5c1e8ead
Robbie Lodico <pandacoder@pm.me>
2022-04-03 02:33:38 -0400

    chore: .NET Standard 2.1 and .NET 6.0 - Ported core library to .netstandard2.1 - Ported example projects to .net6.0 - Updated and tested example projects - Reorganized solution structure

---
+++ Samples/WebSocketSharp.Samples.HttpServer/Echo.cs
@@ -0,0 +1,13 @@
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Example3
+{
+    public class Echo : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Send(e.Data);
+        }
+    }
+}
---
+++ Samples/WebSocketSharp.Samples.SimpleServer/Echo.cs
@@ -0,0 +1,13 @@
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Example2
+{
+    public class Echo : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Send(e.Data);
+        }
+    }
+}
